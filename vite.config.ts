14c08fa7bb04d0d4530b53bd0bd6919c1bf6dd4b
whyleonardo <pleasefloyd73@gmail.com>
2023-03-09 15:04:03 -0300

    chore: change vite server port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 3000
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
