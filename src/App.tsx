9435e6919a5593ca8f79f28a43f92a5ac08c52d4
whyleonardo <pleasefloyd73@gmail.com>
2023-03-10 20:35:38 -0300

    chore: add react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Helmet } from 'react-helmet'
+
 import { Button, Center, useColorMode } from '@chakra-ui/react'
 
 export const App = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <Center h="100vh">
-      <Button onClick={toggleColorMode}>Change Theme</Button>
-    </Center>
+    <>
+      <Helmet title="React + Vite + Chakra UI" />
+
+      <Center h="100vh">
+        <Button onClick={toggleColorMode}>Change Theme</Button>
+      </Center>
+    </>
   )
 }
