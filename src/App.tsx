b22158481e7a2291db51eec212597bd77ff17e7a
whyleonardo <pleasefloyd73@gmail.com>
2023-03-10 10:17:54 -0300

    chore: add all eslint config into a plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Button, Center, useColorMode } from '@chakra-ui/react'
 
 export const App = () => {
-	const { toggleColorMode } = useColorMode()
-	return (
-		<Center h="100vh">
-			<Button onClick={toggleColorMode}>Change Theme</Button>
-		</Center>
-	)
+  const { toggleColorMode } = useColorMode()
+  return (
+    <Center h="100vh">
+      <Button onClick={toggleColorMode}>Change Theme</Button>
+    </Center>
+  )
 }
