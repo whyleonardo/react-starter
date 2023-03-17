1cb4e23c6aa3a03278acff12cba986690ddc63cc
whyleonardo <pleasefloyd73@gmail.com>
2023-03-16 22:46:23 -0300

    feat: add VitePWA plugin to vite.config.ts to enable PWA support chore: update package.json to include vite-plugin-pwa as a dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'My PWA App',
+				short_name: 'My App',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: '/vite.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/vite.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	],
 	server: {
 		port: 3000
 	},
